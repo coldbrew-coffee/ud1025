895a0ef5dcaa72ba290609442c533ec316a377d3
Nicholas Kim <nicholaskim94@gmail.com>
2018-08-13 22:42:25 +0900

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Nicholas Kim on 8/13/18.
+//  Copyright © 2018 Jamong Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
